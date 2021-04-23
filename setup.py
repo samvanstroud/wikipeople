e9130d19571a1c6b9f24ea1c7e4f4f213cdf1f99
samvanstroud <sam.van.stroud@cern.ch>
2021-04-23 14:34:15 +0100

    first commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='wikipeople',
+      version='0.1',
+      description='Get information about people using wikidata',
+      url='http://github.com/svanstroud/wikipeople',
+      author='Sam VS',
+      author_email='sam.van.stround@cern.com',
+      license='MIT',
+      packages=['wikipeople'],
+      zip_safe=False)
